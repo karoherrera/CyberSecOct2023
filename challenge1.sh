9d014b368f2b1cbb526ab0702ccf467c8767ff19
karoherrera <karolinecastilloferrer529@gmail.com>
2023-11-06 19:09:19 -0800

    201-lab1

--- challenge1.sh
+++ challenge1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Script: Ops 201 Class 01 Ops Challenge Solution
-# Author: 
-# Date of latest revision: 
+# Author: Karoline Herrera
+# Date of latest revision: 11/06/2023
 # Purpose: Print a string to the terminal
 echo "Hello Everyyybodyy"
 
