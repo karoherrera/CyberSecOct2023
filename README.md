9835d351edd156b8c7e216602f93d2333e25a67b
karoherrera <karolinecastilloferrer529@gmail.com>
2023-11-06 18:36:59 -0800

    201-lab1

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # CyberSecOct2023
-pleaseeee
---
+++ challenge1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 01 Ops Challenge Solution
+# Author: 
+# Date of latest revision: 
+# Purpose: Print a string to the terminal
