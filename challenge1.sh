2185768f21741f1cb65e8807756005324acd0eff
karoherrera <karolinecastilloferrer529@gmail.com>
2023-11-06 18:41:43 -0800

    201-lab1

--- challenge1.sh
+++ challenge1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Script: Ops 201 Class 01 Ops Challenge Solution
-# Author: 
-# Date of latest revision: 
-# Purpose: Print a string to the terminal
+echo "Ops 201 Class 01 Ops Challenge Solution"
+sleep 1
+echo "Karoline Herrera"\
+sleep 1
+echo "11/06/2023"
+sleep 1
+echo "Printing a string to the terminal"
+
