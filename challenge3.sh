c5aa9fad765c36d3d4640bceec4bfdeb5fb15d85
karoherrera <karolinecastilloferrer529@gmail.com>
2023-11-08 17:25:20 -0800

    201-lab3

--- challenge3.sh
+++ challenge3.sh
@@ -9,10 +9,13 @@ echo "Pick a number between 1 and 9 (0-9)"
 
 read number
 
-if [[ $number = 5 ]]; then    
-    echo "$number is equal to 5"
-elif [[ $number < 5 ]]; then    
-    echo "$number is less than 5"
-elif [[ $number > 5 ]]; then
-    echo "$number is greater than 5"
+if [[ $number = 5 ]]; 
+    then    
+        echo "$number is equal to 5"
+elif [[ $number < 5 ]]; 
+    then    
+        echo "$number is less than 5"
+elif [[ $number > 5 ]]; 
+    then
+        echo "$number is greater than 5"
 fi
