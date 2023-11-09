9fe7159286c08dc5e5baade46256cda1e694c1d7
karoherrera <karolinecastilloferrer529@gmail.com>
2023-11-08 16:25:55 -0800

    Unit 103

---
+++ Unit 103/challenge3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Condtiional Statement 
+# This ops challenges is about conditinal statement of if else statements and how they work
+# We are going to take a varibale with the number and have the computer tell us if its greater than 5 less than 5 or equals 5
+
+number=
+
+echo "Pick a number between 1 and 09 (0-9)"
+
+read number
+
+if [ $number = 05 ]; then
+    echo "$number is equal to 5"
+elif [ $number > 05]; then
+    echo "$number is greater than 5"
+elif [ $number < 05]; then
+    echo "$number is less than 5"
+fi
