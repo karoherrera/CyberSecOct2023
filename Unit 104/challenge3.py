bd7d43528838ef95b8037e7beff6a3540ef458b1
karoherrera <karolinecastilloferrer529@gmail.com>
2023-12-12 16:56:02 -0800

    update

---
+++ Unit 104/challenge3.py
@@ -0,0 +1,46 @@
+# Objectives
+# Create if statements using these logical conditionals below. Each statement should print information to the screen depending on if the condition is met.
+
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+a = 33
+b = 200
+if a < b:
+    print("a is less than b")
+
+
+# Create an if statement using a logical conditional of your choice and include elif keyword that executes when other conditions are not met.
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+# Create an if statement that includes both elif and else to execute when both if and elif are not met.
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+# Stretch Goals (Optional Objectives)
+
+# Pursue stretch goals if you are a more advanced user or have remaining lab time.
+
+# Create an if statement with two conditions by using and between conditions.
+
+# Create an if statement with two conditions by using or between conditions.
+
+# Create a nested if statement.
+
+# Create an if statement that includes pass to avoid errors.
